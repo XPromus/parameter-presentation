b486e99d3ec44f0b875afc9a8f0d6f4d853a6a65
Christopher Hilgner <hilgi45@gmail.com>
2025-08-14 10:24:35 +0200

    Removed documentation link.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,8 +4,5 @@
 
 <div class="flex flex-row w-screen h-screen p-10 space-x-10">
     <Link href="/presentation" text="Presentation" />
-    <div class="flex flex-col space-y-10 basis-1/2">
-        <Link href="/editor" text="Editor" />
-        <Link href="/docs" text="Documentation"/>
-    </div>
+    <Link href="/editor" text="Editor" />
 </div>
