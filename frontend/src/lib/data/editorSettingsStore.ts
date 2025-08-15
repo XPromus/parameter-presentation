83602930e7e7411b56a83ac50c31944339a1d8e7
Christopher Hilgner <hilgi45@gmail.com>
2025-08-15 13:22:27 +0200

    Automatic assignment of pocketbase address and apply button for settings.

--- frontend/src/lib/data/editorSettingsStore.ts
+++ frontend/src/lib/data/editorSettingsStore.ts
@@ -2,6 +2,6 @@ import type { EditorSettings } from "$lib/types/settingsTypes";
 import { writable } from "svelte/store";
 
 export const editorSettings = writable<EditorSettings>({
-    pocketbaseAddress: "http://127.0.0.1:8080",
+    pocketbaseAddress: `http://${window.location.hostname}:8080/`,
     defaultDuration: 10000
 });
--- frontend/src/routes/editor/settings/+page.svelte
+++ frontend/src/routes/editor/settings/+page.svelte
@@ -4,34 +4,24 @@
     import Link from "$lib/components/interaction/Link.svelte";
     import Input from "$lib/components/interaction/Input.svelte";
     import Icon from "@iconify/svelte";
+    import Button from "$lib/components/interaction/Button.svelte";
 
     let defaultDurationInput: number = $state(0);
-    let pocketbaseAddress: string=  $state("http://127.0.0.1:8080");
+    let pocketbaseAddressInput: string=  $state($editorSettings.pocketbaseAddress);
 
-    const onPocketbaseAddressInput = () => {
-        editorSettings.update((value) => {
-            return {
-                pocketbaseAddress: pocketbaseAddress,
-                defaultDuration: value.defaultDuration
-            };
-        })
-    }
-
-    const onDefaultDurationInput = () => {
-        editorSettings.update((value) => {
-            return {
-                pocketbaseAddress: value.pocketbaseAddress,
-                defaultDuration: defaultDurationInput
-            };
+    const onApply = () => {
+        editorSettings.set({
+            pocketbaseAddress: pocketbaseAddressInput,
+            defaultDuration: defaultDurationInput
         });
     }
 
     onMount(() => {
         defaultDurationInput = $editorSettings.defaultDuration;
-    })
+    });
 </script>
 
-<div class="flex flex-col w-full h-full p-5 space-y-5">
+<div class="flex flex-col w-full h-fit p-5 space-y-5">
     <div class="flex flex-row space-x-5 w-full">
         <Link href="/" type="neutral" tooltipOptions={{text: "Back to the startpage", position: "bottom"}}>
             {#snippet children()}
@@ -46,10 +36,15 @@
     </div>
     <div class="flex flex-row w-full space-x-5">
         <span class="text-white">Pocketbase Address</span>
-        <Input onChange={onPocketbaseAddressInput} bind:value={pocketbaseAddress} placeholder="Pocketbase Address" type="text" />
+        <Input bind:value={pocketbaseAddressInput} placeholder="Pocketbase Address" type="text" />
     </div>
     <div class="flex flex-row w-full space-x-5">
         <span class="text-white">Default Duration (ms)</span>
-        <Input onChange={onDefaultDurationInput} bind:value={defaultDurationInput} placeholder="Duration in ms" type="number" />
+        <Input bind:value={defaultDurationInput} placeholder="Duration in ms" type="number" />
     </div>
+    <Button action={onApply} type="success">
+        {#snippet children()}
+            <span>Apply</span>
+        {/snippet}
+    </Button>
 </div>
